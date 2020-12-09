a3978873658a876f9b1c9c1b855bd8800feacfa6
Steffen Park <dev@istherepie.com>
2020-12-09 12:32:47 +0100

    Added solution to part 2 of the day4 challenge

--- solution/day4.go
+++ solution/day4.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 type Passport struct {
@@ -18,8 +20,82 @@ type Passport struct {
 	cid string // (Country ID)
 }
 
-func (p *Passport) isValid() bool {
+func (p *Passport) HasRequiredFields() bool {
 	return p.byr != "" && p.iyr != "" && p.eyr != "" && p.hgt != "" && p.hcl != "" && p.ecl != "" && p.pid != ""
+}
+
+func (p *Passport) ValidateMinMax(value string, min int, max int) bool {
+
+	converted, err := strconv.ParseInt(value, 10, 64)
+
+	if err != nil {
+		return false
+	}
+
+	return int(converted) >= min && int(converted) <= max
+}
+
+func (p *Passport) ValidateHeight(height string) bool {
+	cm := strings.Contains(height, "cm")
+	in := strings.Contains(height, "in")
+
+	if cm {
+		value := strings.Replace(height, "cm", "", 1)
+		return p.ValidateMinMax(value, 150, 193)
+	}
+
+	if in {
+		value := strings.Replace(height, "in", "", 1)
+		return p.ValidateMinMax(value, 59, 76)
+	}
+
+	return false
+}
+
+func (p *Passport) ValidateHairColor(color string) bool {
+	if !strings.HasPrefix(color, "#") {
+		return false
+	}
+
+	value := strings.Replace(color, "#", "", 1)
+
+	for _, char := range value {
+		if unicode.IsSymbol(char) {
+			return false
+		}
+	}
+
+	return len(value) == 6
+}
+
+func (p *Passport) ValidateEyeColor(eyeColor string) bool {
+	required := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+
+	for _, color := range required {
+		if eyeColor == color {
+			return true
+		}
+	}
+	return false
+}
+
+func (p *Passport) IsValidPart1() bool {
+	return p.byr != "" && p.iyr != "" && p.eyr != "" && p.hgt != "" && p.hcl != "" && p.ecl != "" && p.pid != ""
+}
+
+func (p *Passport) IsValidPart2() bool {
+	requirements := p.HasRequiredFields()
+
+	checkLength := len(p.byr) == 4 && len(p.iyr) == 4 && len(p.eyr) == 4 && len(p.pid) == 9
+
+	byr := p.ValidateMinMax(p.byr, 1920, 2002)
+	iyr := p.ValidateMinMax(p.iyr, 2010, 2020)
+	eyr := p.ValidateMinMax(p.eyr, 2020, 2030)
+	hgt := p.ValidateHeight(p.hgt)
+	hcl := p.ValidateHairColor(p.hcl)
+	ecl := p.ValidateEyeColor(p.ecl)
+
+	return requirements && checkLength && byr && iyr && eyr && hgt && hcl && ecl
 }
 
 type Day4 struct {
@@ -45,10 +121,8 @@ func (d *Day4) Input(data io.Reader) error {
 		fields = append(fields, temp...)
 
 		passport := d.CreatePassport(fields)
+		d.Passports[passport] = Empty{}
 
-		if passport.isValid() {
-			d.Passports[passport] = Empty{}
-		}
 	}
 
 	return nil
@@ -85,5 +159,22 @@ func (d *Day4) CreatePassport(fields []string) Passport {
 }
 
 func (d *Day4) Output() {
-	fmt.Printf("=> Answer: %d passports are valid\n", len(d.Passports))
+
+	var validatedPasswordsPart1 int
+	var validatedPasswordsPart2 int
+
+	for passport := range d.Passports {
+		if passport.IsValidPart1() {
+			validatedPasswordsPart1++
+		}
+	}
+
+	for passport := range d.Passports {
+		if passport.IsValidPart2() {
+			validatedPasswordsPart2++
+		}
+	}
+
+	fmt.Printf("=> [PART1] Answer: %d passports are valid\n", validatedPasswordsPart1)
+	fmt.Printf("=> [PART2] Answer: %d passports are valid\n", validatedPasswordsPart2)
 }
